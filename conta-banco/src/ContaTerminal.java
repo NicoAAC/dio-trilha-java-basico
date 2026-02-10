854b1f3cd19e694fe93545f0fc6fc4930dcd95b2
NICOAAC <nichollas.alencar@hotmail.com>
2026-02-09 23:29:20 -0300

    Implementação do sistema para a criação de conta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+import java.util.Scanner;
+public class ContaTerminal { 
+
+    public static void main(String[] args){
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da agência: ");
+        int numeroAgencia = sc.nextInt();
+        System.out.println("Por favor, digite o número da conta: ");
+        int numeroConta = sc.nextInt();
+        System.out.println("Por favor, digite o nome do titular da conta: ");
+        String nomeTitular = sc.next();
+        System.out.println("Por favor, digite o saldo da conta: ");
+        double saldoConta = sc.nextDouble();
+    
+        System.out.println("Parabéns " + nomeTitular + ", sua conta foi criada com sucesso!");
     }
+
+
+
+
+
 }
+    
+
+
+
+
+
